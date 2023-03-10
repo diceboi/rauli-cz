cbe71e3bee40321b42b4f80751f3c64b98152d71
diceboi <szasz.szabolcs1995@gmail.com>
2023-03-10 21:47:25 +0100

    bebas

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="hu">
       <Head>
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
+      <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet"></link>
       </Head>
       <body>
         <Main />
