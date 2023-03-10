be9a82c2a6d4a210f2529f4a40448685f697748f
diceboi <szasz.szabolcs1995@gmail.com>
2023-03-10 21:38:13 +0100

    bebas

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link';
-import { Bebas_Neue } from 'next/font/google'
 
 import { gql } from "@apollo/client";
 import client from "../apollo-client";
