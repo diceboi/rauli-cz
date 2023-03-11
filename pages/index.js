76e02f17883f33a5b77099e5ffec4b63fcb79040
diceboi <szasz.szabolcs1995@gmail.com>
2023-03-12 00:26:30 +0100

    desktop

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Link from 'next/link';
+import Link from 'next/link'
+import { BsCircleFill } from 'react-icons/bs'
+import { BiChevronRight } from 'react-icons/bi'
+import { FaPhoneAlt } from 'react-icons/fa'
 
 import { gql } from "@apollo/client";
 import client from "../apollo-client";
@@ -67,26 +70,31 @@ export default function Home() {
           </div>
         </section>
 
-
-
-        <section id='easyclick'>
-          <div className='grid grid-cols-2'>
-            <div className='flex'>
-              <div className="absolute z-10 col-span-2 w-[1000px] h-[56,25%] bg-center bg-no-repeat bg-auto bg-[url('https://rauli.hu/wp-content/uploads/2022/04/video-utan.jpg')]">
-              </div>
-              <iframe width="100%" height="500px" src="https://www.youtube.com/embed/FzBBb-2valk" title="RAULI - Fastest Mounting" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <section id='easyclick' className='border-t-8 border-[#e94e1b]'>
+          <div className='grid grid-cols-3 bg-neutral-900'>
+            <div className='relative w-full h-auto col-span-2'>
+              <div className='w-full h-full z-10 absolute'>
+                <div id='circle-telescopic' className='absolute top-[25%] left-[40%]'><BsCircleFill className='absolute h-8 w-8 text-[#e94e1b]'/><BsCircleFill className='absolute h-8 w-8 text-[#e94e1b] animate-ping'/></div>
+                <div id='circle-quality' className='absolute top-[50%] left-[39%]'><BsCircleFill className='absolute h-8 w-8 text-[#e94e1b]'/><BsCircleFill className='absolute h-8 w-8 text-[#e94e1b] animate-ping'/></div>
+                <div id='circle-steel' className='absolute top-[40%] left-[55%]'><BsCircleFill className='absolute h-8 w-8 text-[#e94e1b]'/><BsCircleFill className='absolute h-8 w-8 text-[#e94e1b] animate-ping'/></div>
+                <div id='circle-precision' className='absolute top-[35%] left-[75%]'><BsCircleFill className='absolute h-8 w-8 text-[#e94e1b]'/><BsCircleFill className='absolute h-8 w-8 text-[#e94e1b] animate-ping'/></div>
+              </div>  
+              <Image src="https://rauli.hu/wp-content/uploads/2022/04/video-utan.jpg" width={1440} height={960} className="w-full h-auto"/>
+              
             </div>
-
-            <div className='w-auto flex'>
-              <div className="absolute z-10 col-span-2 w-[100%] h-[56,25%] bg-center bg-no-repeat bg-auto bg-[url('https://rauli.hu/wp-content/uploads/2022/04/video-utan.jpg')]">
-              </div>
-              <iframe width="100%" height="56,25%" src="https://www.youtube.com/embed/FzBBb-2valk" title="RAULI - Fastest Mounting" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <div className='flex flex-col gap-4 col-span-1 bg-neutral-900 justify-center items-center'>
+              <button className=' bg-[#e94f1b00] border border-[#e94e1b] px-8 py-2 text-2xl text-white my-6'>
+                <Link href="#fullblack">Nézd meg videón</Link>
+              </button>
+              <h2 className='text-white text-5xl' >Easyclick rendszer</h2>
+              <p className='text-white text-xl' >Extrém gyors szerelhetőség</p>
+              <p className='text-white text-xl' >nincs csavarozás</p>
+              <p className='text-white text-xl' >csak egy mozdulat bepattintani</p>
             </div>
-
           </div>
         </section>
 
-        <section id='warranty' className='flex justify-center items-center h-screen'>
+        <section id='warranty' className='flex justify-center items-center h-[60vh]'>
           <div className='grid grid-row-1 grid-cols-2 min-w-min'>
             <div className='col-span-1 text-8xl text-white'>
               <h1 className='px-4 py-2 bg-[#e94e1b] w-fit'>A Leghosszabb </h1>
@@ -127,7 +135,7 @@ export default function Home() {
           </div>
         </section>
 
-        <section id='extra' className='bg-neutral-900 my-36'>
+        <section id='extra' className='bg-neutral-900'>
           <div className='flex flex-col justify-center items-center w-8/12 min-w-min mx-auto py-36'>
             <h1 className='text-8xl text-white'>Pénz - Idő - Energia</h1>
             <h1 className='text-8xl text-white'>Megtakarítás</h1>
@@ -164,6 +172,81 @@ export default function Home() {
             <h1 className='text-9xl font-black text-neutral-800 mb-[-30px]'>+40%</h1>
             <p className='text-3xl text-white text-center bg-[#e94e1b] w-3/6 py-2 px-4 z-10'>a gyors és egyszerű szerelésnek köszönhetően akár 40%-al több telepítés kivitelezhető ugyanannyi emberrel, vagy kissebb csapat is elég ugyanahhoz a munkához</p>
           </div>
+        </section>
+
+        <section id='workshop' className='h-[60vh] flex'>
+          <div className=' w-8/12 m-auto'>
+            <h1 className='px-4 py-2 bg-[#e94e1b] w-fit text-8xl text-white'>Jelentkezz online</h1>
+            <h1 className='px-4 py-2 bg-[#e94e1b] w-fit text-8xl text-white'>Workshopra</h1>
+            <h3 className='text-3xl my-6'>Tetszik a Rauli rendszer? Megtanulnád az alapjait? Jelentkezz, és mi segítünk neked!</h3>
+            <button className=' bg-[#e94f1b00] border border-[#e94e1b] px-8 py-2 text-2xl'>
+                <Link href="#fullblack" className='flex items-center'>Érdekel <BiChevronRight className='h-10 w-10'/></Link>
+            </button>
+          </div>
+        </section>
+
+        <section id='contact' className='h-screen'>
+          <div className='w-8/12 m-auto'>
+            <h1 className='px-4 py-2 bg-[#e94e1b] w-fit text-8xl text-white'>Írj nekünk!</h1>
+              <form className='grid grid-cols-1 grid-rows-10 gap-4 my-6'>
+                <div className='row-span-1 flex flex-col w-3/6'>
+                  <label className='text-2xl'>Vezetéknév <span className=' text-red-700'>*</span></label>
+                  <input className=' border border-[#e94e1b] p-2' type="text" id='lastname' />
+                </div>
+                <div className='row-span-1 flex flex-col w-3/6'>
+                  <label className='text-2xl'>Keresztnév <span className=' text-red-700'>*</span></label>
+                  <input className=' border border-[#e94e1b] p-2' type="text" id='firstname' />
+                </div>
+                <div className='row-span-1 flex flex-col w-3/6'>
+                  <label className='text-2xl'>Email <span className=' text-red-700'>*</span></label>
+                  <input className=' border border-[#e94e1b] p-2' type="email" id='email' />
+                </div>
+                <div className='row-span-6 flex flex-col w-3/6'>
+                  <label className='text-2xl'>Rövid üzenet <span className=' text-red-700'>*</span></label>
+                  <textarea className=' border border-[#e94e1b] p-2' rows="10" id='lastname' />
+                </div>
+                <div className='row-span-1 flex flex-col w-fit'>
+                  <input type="submit" className=' bg-[#e94e1b] border border-[#e94e1b] px-8 py-2 text-2xl text-white cursor-pointer'/>  
+                </div>
+              </form>
+          </div>
+        </section>
+
+        <section id='footer' className=' bg-neutral-900'>
+          <div className='w-8/12 m-auto grid grid-cols-3 py-6 justify-center items-center'>
+            <div className='relative col-span-1 flex flex-col justify-center items-center text-center text-white'>
+              <h3 className='text-xl z-10'>Rendelés</h3>
+              <p className=' tracking-widest z-10'>Király Éva</p>
+              <p className=' text-2xl mb-6 z-10'>+36 70 / 645 3325</p>
+              <h3 className='text-xl z-10' >További információk</h3>
+              <p className=' tracking-widest z-10'>Csomós Tibor</p>
+              <p className=' text-2xl mb-6 z-10'>+36 20 / 222 4 222</p>
+              <FaPhoneAlt className='absolute h-full w-auto opacity-10 z-0'/>
+            </div>
+            <div className='col-span-1 flex flex-col justify-center items-center'>
+              <Image src="https://rauli.hu/wp-content/uploads/2022/04/rauli-map-europe.png" width={500} height={500} />
+            </div>
+            <div className='col-span-1 flex flex-col justify-start items-start ml-24 gap-6'>
+              <Link href="https://profigreentech.hu"><Image src="https://rauli.hu/wp-content/uploads/2022/04/PGT-LOGO-white.png" width={110} height={100}/></Link>
+              <Link href=""><Image src="https://rauli.hu/wp-content/uploads/2022/04/pgmlogo.png" width={150} height={100}/></Link>
+              <Link href="https://fullblack.hu"><Image src="https://rauli.hu/wp-content/uploads/2022/04/full-black-logo.png" width={260} height={100}/></Link>
+            </div>
+          </div>
+          <div className='flex flex-col gap-4 justify-center items-center w-full bg-[#e94e1b] py-6'>
+            <h3 className='text-white text-xl'>A profi teto kft. - profi greentech divíziója a rauli termékek kizárólagos disztribútora a közép- és kelet európai régióban:</h3>
+            <ul className='flex text-white text-sm tracking-widest w-6/12 justify-evenly'>
+              <li>Magyarország</li>
+              <li>Csehország</li>
+              <li>Magyarország</li>
+              <li>Szlovákia</li>
+              <li>Ausztria</li>
+              <li>Horvátország</li>
+              <li>Szlovénia</li>
+            </ul>
+            <Link className='text-white opacity-50 underline text-md' href="/adatkezelesi-nyilatkozat" >Adatkezelési nyilatkozat</Link>
+
+          </div>
+
         </section>
 
       </main>
